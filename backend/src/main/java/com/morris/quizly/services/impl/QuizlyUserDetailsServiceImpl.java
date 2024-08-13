0da3d229eb075b43ceb2ebe00892e9d491bfdd23
Wali Morris <walimmorris@gmail.com>
2024-08-12 19:27:15 -0700

    update constants in QuizlyUserDetailsService

--- backend/src/main/java/com/morris/quizly/services/impl/QuizlyUserDetailsServiceImpl.java
+++ backend/src/main/java/com/morris/quizly/services/impl/QuizlyUserDetailsServiceImpl.java
@@ -23,6 +23,17 @@ public class QuizlyUserDetailsServiceImpl implements QuizlyUserDetailsService {
     private final UserRepository userRepository;
     private final MongoTemplate mongoTemplate;
 
+    private static final String _ID = "_id";
+    private static final String ENABLED = "enabled";
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "password";
+    private static final String EMAIL_ADDRESS = "emailAddress";
+    private static final String PASSWORD_RESET_TOKEN = "passwordResetToken";
+    private static final String PASSWORD_RESET_TOKEN_EXPIRY = "passwordResetTokenExpiry";
+    private static final String USER_NOT_FOUND_ERROR = "User not found: ";
+    private static final String SIGNUP_TOKEN_NOT_FOUND = "signup confirmation token not found: ";
+    private static final String PASSWORD_RESET_TOKEN_NOT_FOUND = "password reset token not found: ";
+
     @Autowired
     public QuizlyUserDetailsServiceImpl(UserRepository userRepository, MongoTemplate mongoTemplate) {
         this.userRepository = userRepository;
@@ -32,19 +43,19 @@ public class QuizlyUserDetailsServiceImpl implements QuizlyUserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+                .orElseThrow(() -> new UsernameNotFoundException(USER_NOT_FOUND_ERROR + username));
     }
 
     @Override
     public UserDetails findBySignupToken(String token) {
         return userRepository.findBySignupToken(token)
-                .orElseThrow(() -> new UsernameNotFoundException("signup confirmation token not found: " + token));
+                .orElseThrow(() -> new UsernameNotFoundException(SIGNUP_TOKEN_NOT_FOUND + token));
     }
 
     @Override
     public UserDetails findByPasswordResetToken(String token) {
         return userRepository.findByPasswordResetToken(token)
-                .orElseThrow(() -> new UsernameNotFoundException("password reset token not found: " + token));
+                .orElseThrow(() -> new UsernameNotFoundException(PASSWORD_RESET_TOKEN_NOT_FOUND + token));
     }
 
     @Override
@@ -59,36 +70,36 @@ public class QuizlyUserDetailsServiceImpl implements QuizlyUserDetailsService {
 
     @Override
     public void updateAndSetEncodedPassword(UserDetails user, String password) {
-        Query query = new Query(Criteria.where("username").is(user.getUsername()));
-        Update update = new Update().set("password", password);
+        Query query = new Query(Criteria.where(USERNAME).is(user.getUsername()));
+        Update update = new Update().set(PASSWORD, password);
         mongoTemplate.updateFirst(query, update, UserDetails.class);
     }
 
     @Override
     public void updateEnabledStatus(UserDetails user, boolean enabled) {
-        Query query = new Query(Criteria.where("_id").is(user.getId()));
-        Update update = new Update().set("enabled", enabled);
+        Query query = new Query(Criteria.where(_ID).is(user.getId()));
+        Update update = new Update().set(ENABLED, enabled);
         mongoTemplate.updateFirst(query, update, UserDetails.class);
     }
 
     @Override
     public void updateAndRemovePasswordResetToken(String token) {
-        Query query = new Query(Criteria.where("passwordResetToken").is(token));
-        Update update = new Update().unset("passwordResetToken");
-        update.unset("passwordResetTokenExpiry");
+        Query query = new Query(Criteria.where(PASSWORD_RESET_TOKEN).is(token));
+        Update update = new Update().unset(PASSWORD_RESET_TOKEN);
+        update.unset(PASSWORD_RESET_TOKEN_EXPIRY);
         mongoTemplate.updateFirst(query, update, UserDetails.class);
     }
 
     // TODO: add a counter here, users make mistakes such as email deletes but we need to protect from spam
     @Override
     public void updateAndSetPasswordResetToken(UserDetails user, String token) {
-        Query query = new Query(Criteria.where("emailAddress").is(user.getEmailAddress()));
-        Update update = new Update().set("passwordResetToken", token);
+        Query query = new Query(Criteria.where(EMAIL_ADDRESS).is(user.getEmailAddress()));
+        Update update = new Update().set(PASSWORD_RESET_TOKEN, token);
 
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.HOUR, 1);
         Date expire = calendar.getTime();
-        update.set("passwordResetTokenExpiry", expire);
+        update.set(PASSWORD_RESET_TOKEN_EXPIRY, expire);
 
         mongoTemplate.updateFirst(query, update, UserDetails.class);
     }
